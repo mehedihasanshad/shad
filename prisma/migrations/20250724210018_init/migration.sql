348e1d2ddf47cac6b536d7ae67139b6c667dcc54
Ahsan Habib Akik <syedmirhabib@gmail.com>
2025-07-25 03:01:29 +0600

    chore: remove old SQLite dev.db and clean up for production Postgres

---
+++ prisma/migrations/20250724210018_init/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Resource" (
+    "id" SERIAL NOT NULL,
+    "type" TEXT NOT NULL,
+    "url" TEXT,
+    "filename" TEXT,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+    "public" BOOLEAN NOT NULL DEFAULT false,
+    "uploadedById" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Resource_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "Resource" ADD CONSTRAINT "Resource_uploadedById_fkey" FOREIGN KEY ("uploadedById") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
